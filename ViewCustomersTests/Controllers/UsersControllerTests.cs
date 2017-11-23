a23e2b5ffc3649fce1e6ca5c2627f2dc806f1b93
GILMORE <N3106515@live.tees.ac.uk>
2017-11-23 18:26:24 +0000

    Add project files.

---
+++ ViewCustomersTests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewCustomers.Controllers;
+
+namespace ViewCustomers.Tests.Controllers
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            UsersController controller = new UsersController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            UsersController controller = new UsersController();
+
+            // Act
+            ViewResult result = controller.Details() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            UsersController controller = new UsersController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        public void Edit()
+        {
+            // Arrange
+            UsersController controller = new UsersController();
+
+            // Act
+            ViewResult result = controller.Edit() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        public void Delete()
+        {
+            // Arrange
+            UsersController controller = new UsersController();
+
+            // Act
+            ViewResult result = controller.Delete() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
